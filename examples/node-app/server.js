2aa8bd9bc206d19ee5c06d898be6942456d018dd
Ritika Garg <ritikagarg60814@gmail.com>
2025-10-04 22:01:02 +0530

    server check

--- examples/node-app/server.js
+++ examples/node-app/server.js
@@ -5,5 +5,6 @@ const PORT = 4000; // must match docker -p port mapping
 app.use(express.static("public")); // serve static files
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  //console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
